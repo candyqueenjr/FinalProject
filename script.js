5951af96ac2065d0e2b88b031be1b118ec662ab6
candyqueenjr <vardosanidzeano@gmail.com>
2022-11-29 17:37:07 +0400

    bbb

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
-
-//active class fro navigation
+//#region active class for navigation
 
 let navLink = document.querySelectorAll(".nav-link")
 navLink.forEach( a =>{
@@ -10,7 +9,11 @@ navLink.forEach( a =>{
     })
 })
 
+//#endregion
 
+
+
+//#region Logo eyes
 //Logo eyes
 
 window.requestAnimFrame = (function() {
@@ -99,10 +102,11 @@ var bindEventHandlers = function() {
 
 init();
 
+//#endregion
 
 
 
-//header typewritter
+//#region header typeWriter
 
 var _CONTENT = [ "Explode", "Blow up", "Detonate", "Boom" ];
 
@@ -133,47 +137,135 @@ function Type() {
 	}
 }
 
-// Implements deleting effect
 function Delete() {
 	var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
 	_ELEMENT.innerHTML = text;
 	_PART_INDEX--;
 
-	// If sentence has been deleted then start to display the next sentence
 	if(text === '') {
 		clearInterval(_INTERVAL_VAL);
 
-		// If last sentence then display the first one, else move to the next
 		if(_PART == (_CONTENT.length - 1))
 			_PART = 0;
 		else
 			_PART++;
 		_PART_INDEX = 0;
 
-		// Start to display the next sentence after some time
 		setTimeout(function() {
 			_INTERVAL_VAL = setInterval(Type, 150);
 		}, 200);
 	}
 }
 
-// Start the typing effect on load
 _INTERVAL_VAL = setInterval(Type, 150);
 
+//#endregion
 
 
 
-//moving eyes
+//#region Timer
 
-//  document.querySelector("body").addEventListener('mousemove', eyeball);
-//        function eyeball(){
-//         var eye = document.querySelectorAll(".eye");
-//         eye.forEach(function (eye) {
-//         let x = (eye.getBoundingClientRect().left) + (eye.clientWidth / 2); 
-//         let y= (eye.getBoundingClientRect().top) + (eye.clientHeight / 2); 
-//         let radian = Math.atan2(event.pageX - x,event.pageY - y);
-//         let rot = (radian * (180 / Math.PI) * -1) + 270;
-//         eye.style.transform = "rotate("+ rot +"deg)";
-//           })
-           
-//        }
+
+(function () {
+	const second = 999,
+	  minute = second * 60,
+	  hour = minute * 60,
+	  day = hour * 24;
+  
+	let today = new Date(),
+	  dd = String(today.getDate()).padStart(2, "0"),
+	  mm = String(today.getMonth() + 1).padStart(2, "0"),
+	  yyyy = today.getFullYear(),
+	  nextYear = yyyy + 1,
+	  dayMonth = "10/18/",
+	  birthday = dayMonth + yyyy;
+  
+	today = mm + "/" + dd + "/" + yyyy;
+	if (today > birthday) {
+	  birthday = dayMonth + nextYear;
+	}
+
+	
+	const countDown = new Date(birthday).getTime(),
+	  x = setInterval(function () {
+		const now = new Date().getTime(),
+		  distance = countDown - now;
+  
+		(document.getElementById("days").innerText = Math.floor(distance / day)),
+		  (document.getElementById("hours").innerText = Math.floor(
+			(distance % day) / hour
+		  )),
+		  (document.getElementById("minutes").innerText = Math.floor(
+			(distance % hour) / minute
+		  )),
+		  (document.getElementById("seconds").innerText = Math.floor(
+			(distance % minute) / second
+		  ));
+  
+		if (distance < 0) {
+		  document.getElementById("headline").innerText = "All BOOKS are BOOM";
+		  document.getElementById("countdown").style.display = "none";
+		  clearInterval(x);
+		}
+	  }, 0);
+  })();
+  
+  //#endregion
+
+
+
+
+const API_URL = 'https://www.googleapis.com/books/v1/volumes?q=time&printType=books&key=AIzaSyB6UbkiRzJpKT26g1WLRTiBjDl0J90SCe0'
+// const IMG_PATH = ''
+// const SEARCH_URL = 'https://www.googleapis.com/books/v1/volumes?q=search+terms'
+
+
+const main = document.getElementById('main')
+
+getBooks(API_URL)
+
+
+async function getBooks(url) {
+	const res = await fetch(url)
+	const data = await res.json()
+
+	showBooks(data)
+}
+
+function showBooks(books) {
+	main.innerHTML = ''
+
+	books.forEach((book) => {
+		const { title, poster_path, vote_average, overview} = book
+
+		const bookEl = document.createElement
+		('div')
+		bookEl.classList.add('book')
+
+		bookEl.innerHTML = `
+          <img
+            src="${IMG_PATH + poster_path}"
+			alt = "${title}"
+          />
+          <div class="book-info">
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            <h3>${title}</h3>
+          </div>
+          <div class="overview">
+            <h3>overview</h3>
+			${overview}
+          </div>
+		`
+		main.appendChild(bookEl)
+	})
+}
+
+function getClassByRate (vote) {
+	if(vote >= 8) {
+		return 'green'
+	} else if(vote >=5) {
+		return 'orange'
+	} else {
+		return 'red'
+	}
+}
